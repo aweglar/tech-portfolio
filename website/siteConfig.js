6b684a4af16da69adb58fe133b7537565125d897
aweglar <94644589+aweglar@users.noreply.github.com>
2022-11-12 11:50:44 -0500

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -14,7 +14,7 @@ const users = [];
 const siteConfig = {
     title: 'Allyson Weglar', // Title for your website.
     tagline: 'Adaptable, sharp, dedicated software developer',
-    profilePhoto: 'img/profile.png',
+    profilePhoto: 'img/Profilepic.jpg',
     url: 'https://aweglar.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
